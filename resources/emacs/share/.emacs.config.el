1b96f87724372c4a4197e5f45550ff81b6a14ee6
Alexandre Laurent <shaoner@gmail.com>
2014-11-11 11:39:57 +0100

    emacs: Update indent modes

--- resources/emacs/share/.emacs.config.el
+++ resources/emacs/share/.emacs.config.el
@@ -30,6 +30,52 @@
     (save-buffer)
     (call-process "chmod" nil t nil "+x" (buffer-file-name))))
 
+(defun tab-indent-mode (default)
+  (let (setx)
+    (if default
+        (setq setx 'set-default)
+      (setq setx 'set))
+    (funcall setx 'indent-tabs-mode t)
+    (funcall setx 'c-basic-offset 8)
+    (funcall setx 'sh-basic-offset 8)))
+
+(defun space-indent-mode (default)
+  (let (setd)
+    (if default
+        (setq setx 'set-default)
+      (setq setx 'set))
+    (funcall setx 'indent-tabs-mode nil)
+    (funcall setx 'c-basic-offset 4)
+    (funcall setx 'sh-basic-offset 4)))
+
+(defun toggle-indent-mode ()
+  (interactive)
+  (if indent-tabs-mode
+      (progn
+        (message "Space mode")
+        (space-indent-mode nil))
+    (progn
+      (message "Tab mode")
+      (tab-indent-mode nil))))
+
+(defun global-space-indent ()
+  (interactive)
+  (space-indent-mode t)
+  (space-indent-mode nil))
+
+(defun local-space-indent ()
+  (interactive)
+  (apply-partially 'space-indent-mode nil))
+
+(defun global-tab-indent ()
+  (interactive)
+  (tab-indent-mode t)
+  (tab-indent-mode nil))
+
+(defun local-tab-indent ()
+  (interactive)
+  (apply-partially 'tab-indent-mode nil))
+
 ;;-----------------------------------
 ;; PARAMETERS
 ;;-----------------------------------
@@ -84,10 +130,8 @@
 (setq compilation-scroll-output t)
 ;; Enable upcase
 (put 'upcase-region 'disabled nil)
-;; Tabs
-(setq-default tab-width 4)
-(setq indent-tabs-mode t)
-(setq c-basic-offset 4)
+;; Indentation
+(space-indent-mode t)
 
 ;;-----------------------------------
 ;; KEY SHORTCUTS
@@ -124,6 +168,7 @@
 (global-set-key (kbd "<f1>") 'delete-other-windows)
 (global-set-key (kbd "<f2>") 'split-window-horizontally)
 (global-set-key (kbd "<f3>") 'split-window-vertically)
+(global-set-key (kbd "<f4>") 'toggle-indent-mode)
 (global-set-key (kbd "C-x |") 'split-window-horizontally)
 (global-set-key (kbd "C-x \-") 'split-window-vertically)
 ;; Compile
