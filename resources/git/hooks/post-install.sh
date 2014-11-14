1d3b2912915a74c2de1e2bf09c7cd1cc27860a24
Alexandre Laurent <alexandre.laurent@st.com>
2014-11-14 17:55:49 +0100

    git: Update postinstall script

--- resources/git/hooks/post-install.sh
+++ resources/git/hooks/post-install.sh
@@ -1,13 +1,30 @@
 #! /bin/sh
 
-if [ -d ~/.gitconfig.d ]
-then
-    REALNAME=$(grep "^${USER}:" /etc/passwd | cut -d':' -f5)
-    HOSTDOMAIN=$(hostname)
-    EMAIL_ADDR="${USER}@${HOSTDOMAIN}"
-    cat > ~/.gitconfig.d/user.conf <<EOF
+confd=~/.gitconfig.d
+
+if [ ! -f "$confd/user.conf" ]; then
+    realname=$(grep "^$USER:" /etc/passwd | cut -d':' -f5)
+    email="$USER@$(hostname)"
+    while true; do
+        rrealname=$(__input "Your real name [$realname]:")
+        if [ ! -z "$rrealname" ]; then
+            realname=$rrealname
+        fi
+        remail=$(__input "Your email address [$email]:")
+        if [ ! -z "$remail" ]; then
+            email=$remail
+        fi
+        __pr_info "Real name: $realname"
+        __pr_info "Email address: $email"
+        ans=$(__input "Is it correct? [y/N]")
+        if [ "$ans" = "y" ]; then
+            break
+        fi
+    done
+    mkdir -p $confd
+    cat > "$confd/user.conf" <<EOF
 [user]
-	name = ${REALNAME}
-	email = ${EMAIL_ADDR}
+	name = $realname
+	email = $email
 EOF
 fi
